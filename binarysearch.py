1963d45e59efd22c87294ccb1c3d8a5e95d9d2a7
[Andre Albano] <110417140+albano-a@users.noreply.github.com>
2024-05-12 10:22:32 -0300

    chore: Add binary search algorithm implementation in Python

---
+++ binarysearch.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def binarySearch(A, T):
+    n = len(A)
+    L = 0
+    R = n - 1
+
+    while L <= R:
+        m = int(np.floor((L + R) / 2))
+        if A[m] < T:
+            L = m + 1
+        elif A[m] > T:
+            R = m - 1
+        else:
+            return m
+
+    return "Unsuccessful"
+
+
+# Example
+M = np.arange(1, 10, 1)
+
+number = 2
+result = binarySearch(M, number)
+
+print(f"The value of {number} is in index [{result}].")
